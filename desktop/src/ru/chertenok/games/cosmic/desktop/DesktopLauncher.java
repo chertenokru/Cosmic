34371ecc2bd5d48a8b4e3a29e0a55b2c6756ff25
chertenokru <admin@chertenok.ru>
2018-01-12 01:46:49 +0300

    Initial commit

---
+++ desktop/src/ru/chertenok/games/cosmic/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ru.chertenok.games.cosmic.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ru.chertenok.games.cosmic.MyCosmicGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyCosmicGame(), config);
+	}
+}
